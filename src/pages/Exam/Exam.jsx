24abd4ad888893b8a72ddaaaab346ef0a3eb9f4a
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2024-11-15 09:27:05 -0600

    Refactor App component to include Exam page and remove unused code; add Exam component with basic layout

---
+++ src/pages/Exam/Exam.jsx
@@ -0,0 +1,18 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
+
+function Exam() {
+    const json = {};
+
+    return (
+        <Card>
+            <CardBody>
+                <p>
+                    Make beautiful websites regardless of your design
+                    experience.
+                </p>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default Exam;
